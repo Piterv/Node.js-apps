c086c05865ea6c3a9f068588ecbb83ff00bdd0e8
Peter <peter.zgurovets@gmail.com>
2022-07-05 11:42:50 -0700

    Add hashing passwords

--- Authentication $ Security/app.js
+++ Authentication $ Security/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
 
 const app = express();
 
@@ -21,8 +21,8 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-//Add encrypt. 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+//Add encrypt.
+
 
 const User = new mongoose.model('User', userSchema);
 
@@ -39,7 +39,7 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save((err) => {
     if (!err) {
@@ -52,7 +52,8 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
 username = req.body.username;
-password = req.body.password;
+password = md5(req.body.password);
+
 
   User.find({email: username}, (err, foundUser)=>{
     if (err) {
@@ -64,6 +65,8 @@ password = req.body.password;
     };
   });
 });
+
+
 
 app.listen(3000, () => {
   console.log("Server started on port 3000.");
