a5ce94daeb9fead72ad4872ea550613fe141e81c
Peter <peter.zgurovets@gmail.com>
2022-07-06 19:39:24 -0700

    Using Passport.js to Add Cookies and Sessions

--- Authentication $ Security/app.js
+++ Authentication $ Security/app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 const app = express();
 
@@ -15,6 +17,14 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(session({
+  secret:"Our little secret.",
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Connecting to MongoDB.
 mongoose.connect('mongodb://localhost:27017/userDB');
 //Defining Schema.
@@ -22,9 +32,18 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-//Add encrypt.
+
+//Add plugin.
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model('User', userSchema);
+
+// use static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -35,43 +54,47 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
   res.render('register');
 });
+app.get('/secrets', (req, res)=>{
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }else{
+    res.redirect("/login");
+  }
+});
+app.get("/logout", (req, res)=>{
+  req.logout();
+  res.redirect("/");
+});
 
 app.post('/register', (req, res) => {
-
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    // Store hash in your password DB.
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-    newUser.save((err) => {
-      if (!err) {
-        res.render("secrets");
-      } else {
-        console.log(err);
-      }
-    });
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      })
+    }
   });
 });
 
 app.post('/login', (req, res) => {
-  username = req.body.username;
-  password = req.body.password;
 
-  User.find({email: username}, (err, foundUser) => {
-    if (err) {
-    } else {
-      if (foundUser) {
-        // Load hash from your password DB.
-        bcrypt.compare(password, foundUser[0].password, function(err, result) {
-          // result == true
-          if (result == true) {
-              res.render('secrets');
-          }
-        });
-      };
-    };
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
   });
+
+  req.login(user, (err)=>{
+    if (err) {
+      console.log(err);
+    }else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+    }
+  });
+
 });
 
 
